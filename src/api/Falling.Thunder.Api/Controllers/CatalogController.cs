4a8c597799c36af72bef289307c37f51966f56b1
Andrew Sisson <sissonandrewj@gmail.com>
2024-04-03 18:45:19 -0400

    Adding Startup Data

--- src/api/Falling.Thunder.Api/Controllers/CatalogController.cs
+++ src/api/Falling.Thunder.Api/Controllers/CatalogController.cs
@@ -1,26 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Falling.Thunder.Domain.Catalog;
 using System.Security.Cryptography.X509Certificates;
+using Falling.Thunder.Data;
 
 namespace Falling.Thunder.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class CatalogController : ControllerBase{
+        private readonly StoreContext _context;
+        
+        public CatalogController(StoreContext context){
+            _context = context;
+        }
 
         [HttpGet]
         public IActionResult GetItems()
         {
-            var items = new[]
-            {
-            new Item(name: "Item 1", description: "Description 1", brand: "Brand 1", price: 100.00m ),
-            new Item(name: "Item 2", description: "Description 2", brand: "Brand 2", price: 200.00m ),
-            new Item(name: "Item 3", description: "Description 3", brand: "Brand 3", price: 300.00m ),
-            new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m ),
-            new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m ),
-            };
+            return Ok(_context.Items);
 
-            return Ok(items);
         }
 
         [HttpGet("{id:int}")]
@@ -49,6 +47,13 @@ namespace Falling.Thunder.Api.Controllers
                 return BadRequest();
             }
 
+            var existingItem = _context.Items.Find(id);
+            if (existingItem == null){
+                return NotFound();
+            }
+
+            _context.Entry(existingItem).CurrentValues.SetValues(item);
+            _context.SaveChanges();
             return Ok(item);   
                 
              }
--- src/api/Falling.Thunder.Api/Program.cs
+++ src/api/Falling.Thunder.Api/Program.cs
@@ -1,7 +1,15 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Falling.Thunder.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite("Data Source=../store.db",
+    m => m.MigrationsAssembly("Falling.Thunder.Api"));
+});
 
 builder.Services.AddSwaggerGen(c =>
 {
