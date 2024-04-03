bae15db9c34508b0081c2a699debf487dd2b0a2a
Andrew Sisson <sissonandrewj@gmail.com>
2024-04-03 17:22:10 -0400

    Adding Swagger OpenAPI and controllers

--- src/api/Falling.Thunder.Api/Program.cs
+++ src/api/Falling.Thunder.Api/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Falling Thunder API", Version = "v1"});
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Falling Thunder API V1");
+});
+
+app.MapControllers();
 
 app.Run();
